6c3c28a2a94ff71fc9b8b22780e65dbfdbf9775b
popas90 <bogdititupopa@gmail.com>
2017-11-05 12:00:15 +0200

    using env var to access db

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ class App extends Component {
     this.getUsers();
   }
   getUsers() {
-    axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
+    axios.get(`http://${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
     .then((res) => { this.setState({ users: res.data.data.users }); })
     .catch((err) => { console.log(err); })
   }
